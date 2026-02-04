0e197432ffedd8e4984f69d89ebb46cfc14634f9
ars <nurkasymovarsen06@gmail.com>
2026-02-04 17:18:43 +0500

    add auth routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,2 +1,8 @@
+import express from "express";
+import { login, signup } from "../controllers/auth.controller";
 
+const router = express.Router();
 
+router.post("/signup", signup)
+router.post("/login", login)
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import { PrismaClient } from "@prisma/client"
 import dotenv from "dotenv"
 import applicationRouter from "./routes/applications.routes";
+import authRouter from "./routes/auth.routes"
 dotenv.config()
 const app = express();
 const prisma = new PrismaClient();
@@ -11,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/applications", applicationRouter)
-
+app.use("/api/auth", authRouter)
 app.listen(PORT, () => {
     console.log(`Cервер запущен на http://localhost:${PORT}`)
 })
