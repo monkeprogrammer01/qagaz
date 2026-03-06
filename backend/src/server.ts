0c2d4e1538d3ca2028381a23c02ac868116744e1
arsen.seitkali@almatykitap.kz <arsen.seitkali@almatykitap.kz>
2026-03-07 01:09:35 +0500

    add frontend url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,8 +6,14 @@ import authRouter from "./routes/auth.routes"
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT
+const allowed = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    process.env.FRONTEND_URL,
+].filter(Boolean);
+
 app.use(cors({
-    origin: 'http://localhost:5173',  
+    origin: allowed as any,  
     credentials: true                  
 }));
 app.use(express.json())
