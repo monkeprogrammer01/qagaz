b40b2b22a7d46b070f1040576443a465e9aad649
arsen.seitkali@almatykitap.kz <arsen.seitkali@almatykitap.kz>
2026-03-07 01:43:43 +0500

    delete initSimpleData

--- frontend/api/admin.js
+++ frontend/api/admin.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.EXPRESS_APP_API_URL || "http://localhost:5001/api",
+  baseURL: import.meta.env.EXPRESS_APP_API_URL,
   withCredentials: true, 
 });
 
